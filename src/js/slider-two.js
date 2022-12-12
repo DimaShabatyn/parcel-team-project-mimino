822494c8f43b823013d96e4a6403e5e5ea657f70
Malberee <82259650+Malberee@users.noreply.github.com>
2022-12-13 00:41:45 +0200

    tablet version хочу спать

--- src/js/slider-two.js
+++ src/js/slider-two.js
@@ -1,69 +1,11 @@
-// // const swiper1 = new Swiper('.swiper', {
-// //   setWrapperSize: true,
-// //   loop: true,
-// //   width: 194,
-// //   spaceBetween: 16,
-// //   slidesPerView: 1,
-// //   slidesPerGroup: 1,
-// //   initialSlide: 0,
-// //   freeMode: true,
-// //   // slidesOffsetAfter: 0,
-// //   // slidesOffsetBefore: 0,
-// //   createElements: true,
-
-// //   // breakpoints: {
-// //   //   1024: {
-// //   //     slidesPerView: 2,
-// //   //   },
-// //   // },
-
-// //   navigation: {
-// //     nextEl: '.swiper-button-next',
-// //     prevEl: '.swiper-button-prev',
-// //   },
-
-// //   scrollbar: {
-// //     el: '.swiper-scrollbar',
-// //     enabled: false,
-// //   },
-// // });
-
-// const swiper1 = new Swiper('.swiper', {
-//   // speed: 400,
-//   initialSlide: 0,
-
-//   centeredSlides: true,
-//   spaceBetween: 16,
-//   loop: true,
-//   // slidesShadow: true,
-//   // shadowOffset: 60,
-//   // shadowScale: 2,
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
-//   },
-
-//   breakpoints: {
-//     320: {
-//       slidesPerView: 1.3,
-//       spaceBetween: 16,
-//     },
-//     1024: {
-//       slidesPerView: 2,
-//       spaceBetween: 16,
-//     },
-//   },
-// });
-
 const swiper1 = new Swiper('.swiper', {
   // Default parameters
   loop: true,
   freeMode: true,
-  slidesPerView: 1,
+  // slidesPerView: 1,
   initialSlide: 0,
-  centeredSlides: false,
-  spaceBetween: 16,
-  width: 420,
+  slidesPerView: 2,
+  // centeredSlides: true,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
@@ -72,18 +14,13 @@ const swiper1 = new Swiper('.swiper', {
   breakpoints: {
     // when window width is >= 320px
     320: {
-      slidesPerView: 2,
-      spaceBetween: 20,
+      width: 404,
+      spaceBetween: 16,
     },
     // when window width is >= 480px
-    480: {
-      slidesPerView: 3,
-      spaceBetween: 30,
-    },
-    // when window width is >= 640px
-    640: {
-      slidesPerView: 4,
+    1024: {
       spaceBetween: 40,
+      width: 428,
     },
   },
 });
