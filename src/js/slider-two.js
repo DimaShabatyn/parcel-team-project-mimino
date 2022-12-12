6b8952e70a1c224cb07dd6e98d15a9b552bd452b
AlexLukashenin <Alukashenin@gmail.com>
2022-12-12 21:08:53 +0200

    Add to styles

---
+++ src/js/slider-two.js
@@ -0,0 +1,14 @@
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 2,
+    spaceBetween: 20,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  });
