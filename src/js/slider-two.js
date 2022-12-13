a844b4b8419922d5b430a9a7263c8a0c5b1933fd
AlexLukashenin <Alukashenin@gmail.com>
2022-12-13 15:35:34 +0200

    seve

--- src/js/slider-two.js
+++ src/js/slider-two.js
@@ -1,14 +1,27 @@
 
-const swiper3 = new Swiper('.swiper', {
-    // Optional parameters
-    loop: true,
-    slidesPerView: 1,
-    spaceBetween: 20,
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+const swiper4 = new Swiper('.restaurant-about__swiper', {
+  // Default parameters
+  loop: true,
+  freeMode: true,
+  // slidesPerView: 1,
+  initialSlide: 0,
+  slidesPerView: 2,
+  // centeredSlides: true,
+  navigation: {
+    nextEl: '.restaurant-about__btn-prev',
+    prevEl: '.restaurant-about__btn-nex',
+  },
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      spaceBetween: 25,
+      width: 330,
     },
-  
-  });
+    // when window width is >= 480px
+    1024: {
+      spaceBetween: 40,
+      width: 548,
+    },
+  },
+});
