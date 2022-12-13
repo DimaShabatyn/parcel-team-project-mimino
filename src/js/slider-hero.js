68a0e2b2af0161129ceae1f12c72edbf4939c99c
VictorPodhaniuk <thevipo777@gmail.com>
2022-12-14 01:17:36 +0200

    add slider/arrow

--- src/js/slider-hero.js
+++ src/js/slider-hero.js
@@ -4,18 +4,18 @@ const swiper7 = new Swiper('.swiper', {
   freeMode: false,
   // slidesPerView: 1,
   initialSlide: 0,
-  slidesPerView: 1,
+  slidesPerView: 0,
   // centeredSlides: true,
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.hero-room__swiper-button-next',
+    prevEl: '.hero-room__swiper-button-prev',
   },
   // Responsive breakpoints
   breakpoints: {
     // when window width is >= 320px
     320: {
       spaceBetween: 0,
-      width: 1024,
+      width: 414,
     },
     // when window width is >= 480px
     1024: {
