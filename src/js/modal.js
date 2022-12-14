2001615fec591b7d00d1647460ae28132dd7f193
WerewolfDevLearn <werewolfdevlearn@gmail.com>
2022-12-15 00:14:57 +0200

    Update modal.js

--- src/js/modal.js
+++ src/js/modal.js
@@ -6,20 +6,26 @@ const refs = {
   backdropRm: document.querySelector('[data-modal-room-in]'),
   closeBtnRm: document.querySelector('[data-modal-room-close]'),
   modalRoom: document.querySelector('.modal-room'),
-  // backdropPrcL: document.querySelector('[data-modal-price-in-l]'),
+};
+
+const refsPrice = {
+  backdropPrcL: document.querySelector('[data-modal-price-in-l]'),
   // backdropPrcD: document.querySelector('[data-modal-price-in-d]'),
   // backdropPrcT: document.querySelector('[data-modal-price-in-t]'),
-  // modalPrcL: document.querySelector('[data-modal-price-l]'),
+  modalPrcL: document.querySelector('[data-modal-price-l]'),
   // modalPrcD: document.querySelector('[data-modal-price-d]'),
   // modalPrcT: document.querySelector('[data-modal-price-t]'),
-  // closeBtnPrcL: document.querySelector('[data-modal-price-close-l]'),
+  closeBtnPrcL: document.querySelector('[data-modal-price-close-l]'),
   // closeBtnPrcD: document.querySelector('[data-modal-price-close-d]'),
   // closeBtnPrcT: document.querySelector('[data-modal-price-close-t]'),
 };
 
+console.log(refsPrice.backdropPrcL);
+console.log(refsPrice.closeBtnPrcL);
+console.log(refsPrice.modalPrcL);
+
 refs.body.addEventListener('click', showBackDrop);
 
-console.log(refs.closeBtnTbl);
 function showBackDrop(e) {
   if (e.target.nodeName !== 'BUTTON') {
     console.log(e.target);
@@ -44,20 +50,6 @@ function showBackDrop(e) {
     refs.modalRoom.classList.remove('is-hidden');
     return;
   }
-
-  // console.log(
-  //   e.target.className === 'adult-third__button button' &&
-  //     e.target.textContent === 'Переглянути ціни'
-  // );
-
-  // if (
-  //   e.target.className === 'adult-third__button button' &&
-  //   e.target.textContent === 'Переглянути ціни'
-  // ) {
-  //   refs.backdropPrcL.classList.remove('is-hidden');
-  //   refs.modalPrcL.classList.remove('is-hidden');
-  //   return;
-  // }
 }
 
 if (refs.closeBtnTbl) {
@@ -77,11 +69,27 @@ function closeBackDropTable(e) {
   refs.modalTable.classList.add('is-hidden');
 }
 
-// function closeBackDropPrcL(e) {
-//   refs.backdropPrcL.classList.add('is-hidden');
-//   refs.modalPrcL.classList.add('is-hidden');
-// }
+function closeBackDropPrcL(e) {
+  refsPrice.backdropPrcL.classList.add('is-hidden');
+  refsPrice.modalPrcL.classList.add('is-hidden');
+}
 
 // if (refs.closeBtnPrcL) {
 //   refs.closeBtnPrcL.addEventListener('click', closeBackDropPrcL);
 // }
+
+// console.log(
+//   e.target.className === 'adult-third__button button' &&
+//     e.target.textContent === 'Переглянути ціни'
+// );
+
+// if (
+//   e.target.className === 'adult-third__button button' &&
+//   e.target.textContent === 'Переглянути ціни'
+// ) {
+//   refs.backdropPrcL.classList.remove('is-hidden');
+//   refs.modalPrcL.classList.remove('is-hidden');
+//   return;
+// }
+
+console.log(refsPrice.backdropPrcL);
