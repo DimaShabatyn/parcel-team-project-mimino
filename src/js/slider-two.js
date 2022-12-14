4bf1f3a0ca7cf9b89b744f029667aba57e505be1
AlexLukashenin <Alukashenin@gmail.com>
2022-12-14 13:01:47 +0200

    added a slider

--- src/js/slider-two.js
+++ src/js/slider-two.js
@@ -5,28 +5,32 @@ const swiper4 = new Swiper('.swiper', {
   freeMode: true,
   // slidesPerView: 1,
   initialSlide: 0,
-  slidesPerView: 2,
+  // slidesPerView: 2,
   // centeredSlides: true,
   navigation: {
-    nextEl: '.swiper-button-prev',
-    prevEl: '.swiper-button-next',
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
   // Responsive breakpoints
   breakpoints: {
     // when window width is >= 320px
     320: {
       spaceBetween: 25,
-      width: 533,
+      slidesPerView: 1.315,
+      // width: 533,
+      width: 330,
     },
     // when window width is >= 480px
     1024: {
       spaceBetween: 40,
+      slidesPerView: 2,
       width: 428,
        },
    // when window width is >= 480px
    1280: {
     spaceBetween: 40,
     width: 548,
+    slidesPerView: 2,
   },
 },
 });
