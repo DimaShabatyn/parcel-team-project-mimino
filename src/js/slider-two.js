c544aaff21319393ddd07891a21e981d282e5fd5
AlexLukashenin <Alukashenin@gmail.com>
2022-12-14 15:47:16 +0200

    del js

--- src/js/slider-two.js
+++ src/js/slider-two.js
@@ -1,50 +1,9 @@
-<<<<<<< HEAD
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const swiper4 = new Swiper('.swiper', {
-=======
-const swiper3 = new Swiper('.swiper', {
->>>>>>> main
   // Default parameters
   loop: true,
   freeMode: true,
   // slidesPerView: 1,
   initialSlide: 0,
-<<<<<<< HEAD
-  // slidesPerView: 2,
-=======
-  slidesPerView: 2,
->>>>>>> main
-  // centeredSlides: true,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
@@ -53,20 +12,14 @@ const swiper3 = new Swiper('.swiper', {
   breakpoints: {
     // when window width is >= 320px
     320: {
-<<<<<<< HEAD
       spaceBetween: 25,
       slidesPerView: 1.315,
       // width: 533,
       width: 330,
-=======
-      spaceBetween: 16,
-      width: 404,
->>>>>>> main
     },
     // when window width is >= 480px
     1024: {
       spaceBetween: 40,
-<<<<<<< HEAD
       slidesPerView: 2,
       width: 428,
        },
@@ -77,13 +30,4 @@ const swiper3 = new Swiper('.swiper', {
     slidesPerView: 2,
   },
 },
-=======
-      width: 428,
-    },
-    1280: {
-      spaceBetween: 40,
-      width: 548,
-    },
-  },
->>>>>>> main
 });
