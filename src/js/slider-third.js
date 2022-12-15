c46cbfa72c45fe6fa62e0ff38b35023e6fc973f5
SerhiiMaslov1979 <112109122+SerhiiMaslov1979@users.noreply.github.com>
2022-12-15 21:39:16 +0200

    hotel-details finish

--- src/js/slider-third.js
+++ src/js/slider-third.js
@@ -18,6 +18,42 @@ const swiper5 = new Swiper('.restaurant-detailes__swiper', {
     //   // when window width is >= 320px
     320: {
       slidesPerView: 1.3,
+      width: 375,
+    },
+    //   // when window width is >= 480px
+    1024: {
+      slidesPerView: 2.6,
+      spaceBetween: 34,
+      width: 662,
+    },
+    // when window width is >= 480px
+    1280: {
+      slidesPerView: 2.6,
+      spaceBetween: 86,
+      width: 874,
+    },
+  },
+});
+const swiper12 = new Swiper('.hotel-details__swiper', {
+  // Default parameters
+  loop: true,
+  freeMode: true,
+  spaceBetween: 20,
+  initialSlide: 0,
+  slidesPerView: 3,
+  // slidePerGroup: 3,
+  // width: 881,
+  centeredSlides: true,
+  centeredSlidesBounds: false,
+  navigation: {
+    nextEl: '.hotel-details__swiper-button-next',
+    prevEl: '.hotel-details__swiper-button-prev',
+  },
+  // Responsive breakpoints
+  breakpoints: {
+    //   // when window width is >= 320px
+    320: {
+      slidesPerView: 1.3,
       width: 374,
     },
     //   // when window width is >= 480px
@@ -30,7 +66,7 @@ const swiper5 = new Swiper('.restaurant-detailes__swiper', {
     1280: {
       slidesPerView: 2.6,
       spaceBetween: 86,
-      width: 875,
+      width: 874,
     },
   },
 });
