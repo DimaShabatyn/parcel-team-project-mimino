e24361ecd7aa45883cbb5fdbd844841506a6a3fa
VictorPodhaniuk <thevipo777@gmail.com>
2022-12-15 23:58:25 +0200

    corr. js

--- src/js/slider-hero.js
+++ src/js/slider-hero.js
@@ -1,30 +1,14 @@
 const swiper7 = new Swiper('.swiper', {
   // Default parameters
   loop: true,
+  loopedSlide: 3,
+  // allowSlideNext: true,
   freeMode: false,
   initialSlide: 0,
-  slidesPerView: auto,
-  loop: true,
-  centeredSlides: true,
+  slidesPerView: 1,
+  // centeredSlides: true,
   navigation: {
     nextEl: '.hero-room__swiper-button-next',
     prevEl: '.hero-room__swiper-button-prev',
-  },
-  // Responsive breakpoints
-  breakpoints: {
-    // when window width is >= 320px
-    320: {
-      spaceBetween: 0,
-      width: 414,
-    },
-    1024: {
-      spaceBetween: 0,
-      width: 1024,
-    },
-    // when window width is >= 480px
-    1280: {
-      spaceBetween: 0,
-      width: 1280,
-    },
   },
 });
