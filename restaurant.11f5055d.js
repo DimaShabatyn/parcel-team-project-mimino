6178f83b261e61e1d5164bfd309654e4382d6ae9
DimaShabatyn <112180483+DimaShabatyn@users.noreply.github.com>
2022-12-16 01:11:15 +0000

    Deploying to gh-pages from @ DimaShabatyn/parcel-team-project-mimino@0985734a6f661e937ee425b977ad0abb77ea5e2d 🚀

---
+++ restaurant.11f5055d.js
@@ -0,0 +1,2 @@
+var swiper7=new Swiper(".swiper",{loop:!0,loopedSlide:3,freeMode:!1,initialSlide:0,slidesPerView:1,navigation:{nextEl:".hero-room__swiper-button-next",prevEl:".hero-room__swiper-button-prev"}});
+//# sourceMappingURL=restaurant.11f5055d.js.map
